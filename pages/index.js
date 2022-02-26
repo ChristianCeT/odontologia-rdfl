ac54906c5ccf81efd72d0ecd5c54c407008da496
ChristianCeT <chrisatnat@hotmail.com>
2022-02-26 05:38:59 -0500

    v10.1.11

--- pages/index.js
+++ pages/index.js
@@ -15,22 +15,16 @@ const index = () => {
           key="viewport"
         />
         <meta title="Rodo Lab | Laboratorio dental"></meta>
-        <meta name="keywords" content="diente, laboratorio, dientes"/>
         <meta
           name="description"
           content="En Rodo Lab podemos construir y hacer toda clase de dientes, permitiendo una mejor comodidad y apoyando al bienestar de la salud"
         ></meta>
-        <meta name="robots" content="index, follow"></meta> 
+        <meta name="robots" content="index, follow"></meta>
         <meta
           property="og:title"
           content="Rodo Lab | Laboratorio dental"
         ></meta>
-        <meta
-          property="og:url"
-          content="https://odontologia-rdfl.vercel.app/"
-        ></meta>
-        <meta property="og:type" content="website"></meta>
-        
+        <meta property="og:url" content="https://www.rdlflab.com"></meta>
       </Head>
       <Header></Header>
       <Body></Body>
