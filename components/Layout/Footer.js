b3adf60b9b812bdf6e92c403142e7f8a2f64d5c8
Christian Alvaro Cervantes Tanta <76827333+ChristianCeT@users.noreply.github.com>
2022-03-04 21:17:10 -0500

    v12.0.0

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -37,7 +37,7 @@ const Footer = () => {
     } else {
       await emailjs
         .send(
-          "service_cb2kld5",
+          "service_yogp0se",
           "template_zob5jh9",
           formDatos,
           "user_L5vVaEPsXKcGpeoAsMoZy"
